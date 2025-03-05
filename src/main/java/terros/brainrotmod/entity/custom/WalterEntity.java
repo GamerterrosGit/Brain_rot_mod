ba6679255b41613c08f049d867b1945733f71020
GamerterrosGit <87456952+GamerterrosGit@users.noreply.github.com>
2025-03-05 16:53:49 +0530

    Walter now spawns naturally in dark forests

--- src/main/java/terros/brainrotmod/entity/custom/WalterEntity.java
+++ src/main/java/terros/brainrotmod/entity/custom/WalterEntity.java
@@ -1,37 +1,22 @@
 package terros.brainrotmod.entity.custom;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ExperienceOrbEntity;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.*;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.MerchantEntity;
-import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOfferList;
 import net.minecraft.village.TradedItem;
-import net.minecraft.world.BlockRenderView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import org.jetbrains.annotations.Nullable;
-import terros.brainrotmod.block.ModBlocks;
 import terros.brainrotmod.item.ModItems;
 
 public class WalterEntity extends WanderingTraderEntity {
@@ -46,14 +31,14 @@ public class WalterEntity extends WanderingTraderEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(1, new StopFollowingCustomerGoal(this));
-        this.goalSelector.add(1, new FleeEntityGoal(this, ZombieEntity.class, 8.0F, 0.5, 5));
-        this.goalSelector.add(1, new FleeEntityGoal(this, EvokerEntity.class, 12.0F, 0.5, 5));
-        this.goalSelector.add(1, new FleeEntityGoal(this, VindicatorEntity.class, 8.0F, 0.5, 5));
-        this.goalSelector.add(1, new FleeEntityGoal(this, VexEntity.class, 8.0F, 0.5, 5));
-        this.goalSelector.add(1, new FleeEntityGoal(this, PillagerEntity.class, 15.0F, 0.5, 5));
-        this.goalSelector.add(1, new FleeEntityGoal(this, IllusionerEntity.class, 12.0F, 0.5, 5));
-        this.goalSelector.add(1, new FleeEntityGoal(this, ZoglinEntity.class, 10.0F, 0.5, 5));
-        this.goalSelector.add(1, new EscapeDangerGoal(this, 5));
+        this.goalSelector.add(1, new FleeEntityGoal(this, ZombieEntity.class, 8.0F, 0.5, 2));
+        this.goalSelector.add(1, new FleeEntityGoal(this, EvokerEntity.class, 12.0F, 0.5, 2));
+        this.goalSelector.add(1, new FleeEntityGoal(this, VindicatorEntity.class, 8.0F, 0.5, 2));
+        this.goalSelector.add(1, new FleeEntityGoal(this, VexEntity.class, 8.0F, 0.5, 2));
+        this.goalSelector.add(1, new FleeEntityGoal(this, PillagerEntity.class, 15.0F, 0.5, 2));
+        this.goalSelector.add(1, new FleeEntityGoal(this, IllusionerEntity.class, 12.0F, 0.5, 2));
+        this.goalSelector.add(1, new FleeEntityGoal(this, ZoglinEntity.class, 10.0F, 0.5, 2));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 2));
         this.goalSelector.add(1, new LookAtCustomerGoal(this));
         this.goalSelector.add(4, new GoToWalkTargetGoal(this, 0.35));
         this.goalSelector.add(8, new WanderAroundFarGoal(this, 0.35));
@@ -87,7 +72,7 @@ public class WalterEntity extends WanderingTraderEntity {
         public static DefaultAttributeContainer.Builder createAttributes() {
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.MAX_HEALTH, 50)
-                .add(EntityAttributes.MOVEMENT_SPEED, 0.35)
+                .add(EntityAttributes.MOVEMENT_SPEED, 0.9)
                 .add(EntityAttributes.ATTACK_DAMAGE, 1);
         }
 //
@@ -108,10 +93,10 @@ public class WalterEntity extends WanderingTraderEntity {
         tradeOfferList.add(customTrade);
 
     }
-    public static boolean isValidNaturalSpawn(EntityType<? extends WalterEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
-        boolean bl = SpawnReason.isTrialSpawner(spawnReason);
-        return world.getBlockState(pos.down()).isIn(BlockTags.ANIMALS_SPAWNABLE_ON) && bl;
-    }
+//    public static boolean isValidNaturalSpawn(EntityType<? extends WalterEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+//        boolean bl = SpawnReason.isTrialSpawner(spawnReason);
+//        return world.getBlockState(pos.down()).isIn(BlockTags.ANIMALS_SPAWNABLE_ON) && bl;
+//    }
 
     @Override
     public void tick() {
--- src/main/java/terros/brainrotmod/world/gen/ModEntitySpawns.java
+++ src/main/java/terros/brainrotmod/world/gen/ModEntitySpawns.java
@@ -5,19 +5,17 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnLocationTypes;
 import net.minecraft.entity.SpawnRestriction;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.WanderingTraderEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.BiomeKeys;
 import terros.brainrotmod.entity.ModEntities;
-import terros.brainrotmod.entity.client.WalterModel;
 import terros.brainrotmod.entity.custom.WalterEntity;
 
 public class ModEntitySpawns {
     public static void addSpawns() {
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.PLAINS, BiomeKeys.DESERT),
-                SpawnGroup.CREATURE, ModEntities.WALTER, 700, 10, 20);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.DARK_FOREST),
+                SpawnGroup.CREATURE, ModEntities.WALTER, 50, 1, 2);
         SpawnRestriction.register(ModEntities.WALTER, SpawnLocationTypes.ON_GROUND,
-                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WalterEntity::isValidNaturalSpawn);
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MobEntity::canMobSpawn);
     }
 }
